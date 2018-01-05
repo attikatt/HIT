36204ec00491615e080d18a7527861f474db7f0f
Anna <anna_ringqvist@yahoo.se>
2018-01-05 09:52:13 +0100

    fixat så ingredienser tas bort då man går tillbaka i välj ing stegen

--- public/js/ordering.js
+++ public/js/ordering.js
@@ -95,6 +95,10 @@ var vm = new Vue({
 			}
       }
     },
+      
+    removeItemFromOrder: function () {
+        this.chosenIngredients.pop();
+    },
 /*------  Making the text of the chosing ingredient so it occurs in the circle-----*/
 // TRIED TO CONTACINATE... LOOK INTO MORE OBS	  
 	ingText: function (ingitem){
