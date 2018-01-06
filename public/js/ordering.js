9cb99ac6e194f40131c35d27238e169b5a4e45ed
fantski <tapper.fanny@gmail.com>
2018-01-06 14:08:19 +0100

    fixat med thanks page, så den visar ordernummer och namn på dryck

--- public/js/ordering.js
+++ public/js/ordering.js
@@ -461,18 +461,18 @@ var vm = new Vue({
       
       showOrderedItems: function(orderNumber) {
           var allOrders = this.orders;
-          console.log(allOrders);
-          console.log(this.fullOrder);
+          console.log(orderNumber);
+          //console.log(this.fullOrder);
           var h4 = document.createElement("h4");
           var nodeRef = document.getElementById("orderedItems");
           
-          var index = Object.keys(allOrders).map(function(e) { return e.orderId}).indexOf(orderNumber);
-          console.log(index);
+          //var index = Object.keys(allOrders).map(function(e) { return e.orderId}).indexOf(orderNumber);
+          //console.log(index);
           
           var name = Object.values(allOrders).name;
-          console.log(name);
+          //console.log(name);
           
-          var text = document.createTextNode(" #" + orderNumber);
+          var text = document.createTextNode(orderNumber[1]+" #" + orderNumber[0]);
           var br = document.createElement("br");
           h4.appendChild(text);
           nodeRef.appendChild(h4);
