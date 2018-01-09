830babfdd135a104af8e6c78f55bd838a3d42f82
Macbook <pro@nl119-179-102.student.uu.se>
2018-01-09 12:11:22 +0100

    Astrids ändringar i stats och test i app

--- public/js/stats.js
+++ public/js/stats.js
@@ -51,40 +51,55 @@ setTimeout(updateClock,1000);
      /*--- Gå igenom ordrar, dess ingredienser och jämför---*/
      for (var i = 1; i < Object.keys(this.orders).length +1; i += 1) { //loopa över alla ordrar
        for (var j = 0; j < this.orders[i].ingredients.length; j++){ //loopa över varje orders ingredienser
-         for (var k = 1; k < contentArr.length; k++){ //loopa över alla ingredienser (för att jämföra)
-           if (contentArr[k][0] == this.orders[i].ingredients[j].ingredient_sv){
+         for (var k = 1; k < contentArr.length; k++){ //loopa över alla ingredienser
+           if (contentArr[k][0] == this.orders[i].ingredients[j].ingredient_sv){ //jämför
              contentArr[k][1] ++;
            }
          }
        }
       }
-      console.log(contentArr.sort());
       /*---Rensa ut ingredienser som inte är beställda tillräckligt ofta--*/
       var m = 1;
       while (m < contentArr.length){
-        if(contentArr[m][1] < 2){
-          console.log("Remove");
+        if(contentArr[m][1] < 1){
           contentArr.splice(m,1);
         } else {m++;}
       }
-      console.log(contentArr);
      return (contentArr)
+   },
+   getCurrentStatus(){
+     return this.orders;
    }
  }
 });
 
-/*-------------------Grafer------------*/
-
+/*--------------Rita grafer------------*/
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 
-//dataVm.createData();
+function getColors(size){
+  var colors = [];
+  var red = 0;
+  var green = 140;
+  var blue = 0;
+  for (var i = 0; i < size; i ++){
+    var currColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    colors.push(currColor);
+    red += 5;
+    green += 2;
+    blue += 5;
+  }
+//  console.log(colors);
+  return colors;
+
+}
+
 function drawChart() {
   var orderData = google.visualization.arrayToDataTable(dataVm.getOrderData());
   var orderOptions = {
     title: 'Fördelning av beställningar',
     pieHole: 0.4,
-    colors: ['blue', 'green'],
+    colors: ['rgb(255,51,153)', 'rgb(220,220,51)'],
     'backgroundColor':'transparent',
     'titleTextStyle': {color:'white', fontName: 'champagne__limousinesregular', fontSize:'20', bold:'false'},
     legend: {textStyle: {color: 'white', fontName: 'champagne__limousinesregular', fontSize:'16'}}
@@ -94,15 +109,15 @@ function drawChart() {
   var ingredientOptions = {
     title: 'Fördelning av beställda ingredienser',
     pieHole: 0.4,
-    colors: ['hotpink', 'limegreen', 'purple', 'yellow','orange'],
+    colors: getColors(dataVm.getIngredientData().length),
+    //colors: ['rgb(0, 140, 0)', 'rgb(255,255,255)'],
     'backgroundColor':'transparent',
     'titleTextStyle': {color:'white', fontName: 'champagne__limousinesregular', fontSize:'20', bold:'false'},
     legend: {textStyle: {color: 'white', fontName: 'champagne__limousinesregular', fontSize:'16'}},
     pieResidueSliceLabel: 'Övriga',
-    pieResidueSliceColor: 'darkgreen',
+    //pieResidueSliceColor: '#365888',
     sliceVisibilityThreshold: 6/100
   };
-
 
   var chart1 = new google.visualization.PieChart(document.getElementById('donutchartOrders'));
   chart1.draw(orderData, orderOptions);
@@ -110,5 +125,19 @@ function drawChart() {
   var chart2 = new google.visualization.PieChart(document.getElementById('donutchartIngred'));
   chart2.draw(ingredientData, ingredientOptions);
 
-  setTimeout(drawChart,1000); //här regleras hur ofta grafen uppdateras, kan behöva sänkas om många klienter
+  /*if (Object.keys(dataVm.getCurrentStatus()).length > 0){
+    var chart1 = new google.visualization.PieChart(document.getElementById('donutchartOrders'));
+    chart1.draw(orderData, orderOptions);
+
+    if (dataVm.getIngredientData().length > 1){
+      var chart2 = new google.visualization.PieChart(document.getElementById('donutchartIngred'));
+      chart2.draw(ingredientData, ingredientOptions);
+    } else  { //denna visas beorende på vad gränsen för sållningen är
+      document.getElementById('donutchartIngred').innerHTML = "<span style='font-size:1.5vw;margin: 5vw;'>Not enough data on ingredients to display chart</span>";
+  }
+
+} else {
+  document.getElementById('VueDiv').innerHTML = "<span style='font-size:4vw;margin: 15vw;'>Not enough recived orders to display data</span>";
+}*/
+  setTimeout(drawChart,1000); //här regleras hur ofta graferna uppdateras, kan behöva sänkas om många klienter
 }
